b260202d83de793321502c71398759107729c33d
iamrohan <vramaraju147@gmail.com>
2020-05-28 22:55:35 -0400

    'redux-saga'

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,13 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+import createSagaMiddleware from 'redux-saga';
+
+import {fetchCollectionsStart} from './shop/shop.sagas'
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
@@ -12,6 +18,8 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(fetchCollectionsStart)
+
 export const persistor = persistStore(store);
 
-export default { store, persistStore };
+export default { store, persistStore }; 
